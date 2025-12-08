2127d9ce9e281c616acd9a8d10e0177a92155244
Bhuvaneswaran R <bhuvaneswaran@Bhuvaneswarans-MacBook-Air.local>
2025-12-08 08:56:36 -0800

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // GitHub Pages + static export can't use Next's image optimizer
+    unoptimized: true,
+  },
+  // Export as static HTML
+  output: "export",
+  // IMPORTANT:
+  // Replace <your-repo-name> below with the actual repository name
+  basePath: isProd ? "/bhuvaneswaran21.github.io" : "",
+  assetPrefix: isProd ? "/bhuvaneswaran21.github.io/" : "",
 };
 
 export default nextConfig;
+
+
+
