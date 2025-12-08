ae4a73a543f48fd1e39c8f05ce8b306ce4d8de34
bhuvaneswaran21 <72143649+bhuvaneswaran21@users.noreply.github.com>
2025-12-08 09:15:30 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,10 @@
-const isProd = process.env.NODE_ENV === "production";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   images: {
-    // GitHub Pages + static export can't use Next's image optimizer
     unoptimized: true,
   },
-  // Export as static HTML
-  output: "export",
-  // IMPORTANT:
-  // Replace <your-repo-name> below with the actual repository name
-  basePath: isProd ? "/bhuvaneswaran21.github.io" : "",
-  assetPrefix: isProd ? "/bhuvaneswaran21.github.io/" : "",
 };
 
 export default nextConfig;
-
-
-
